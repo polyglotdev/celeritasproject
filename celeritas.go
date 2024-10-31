73f3167d549ed85a554dbabfda78f75f37dc5ed9
polyglotdev <dom@domhallan.com>
2024-10-31 15:07:44 -0400

    🐭 add new funtions to celeritas module

--- celeritas.go
+++ celeritas.go
@@ -1,1 +1,26 @@
 package celeritas
+
+const (
+	Version = "1.0.0"
+)
+
+// Celeritas is the main application struct
+type Celeritas struct {
+	AppName string
+	Debug   bool
+	Version string
+}
+
+// New returns a new Celeritas application
+func (c *Celeritas) New(rootPath string) error {
+	c.AppName = "celeritas"
+	c.Debug = true
+	c.Version = Version
+
+	return nil
+}
+
+// Init takes a initPaths and returns an error
+func (c *Celeritas) Init(initPaths ...string) error {
+	return nil
+}
