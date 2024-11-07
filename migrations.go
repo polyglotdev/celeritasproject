0515e545f5d20692d372c86e284a630a8ef64926
Dom Hallan <dom@domhallan.com>
2024-11-07 17:56:11 -0500

    :card_file_box: Added database migration functions
    Introduced several new functions to handle database migrations. These include `MigrateUp` for running all up migrations, `MigrateDownAll` for reverting all migrations, `Steps` for applying a specific number of migrations, and `MigrateForce` to forcibly apply a migration regardless of its current state. Each function logs any errors encountered during the process.

---
+++ migrations.go
@@ -0,0 +1,63 @@
+package celeritas
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func (c *Celeritas) MigrateUp(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err = m.Up(); err != nil {
+		log.Println("Error running migration:", err)
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) MigrateDownAll(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Down(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) Steps(n int, dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Steps(n); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) MigrateForce(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Force(-1); err != nil {
+		return err
+	}
+
+	return nil
+}
