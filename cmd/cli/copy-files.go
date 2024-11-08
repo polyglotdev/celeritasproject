28bf31a9d2f24fd1105ef466e89cce8fb097f6a3
Dom Hallan <dom@domhallan.com>
2024-11-07 20:44:48 -0500

    🔄 Refactored file copying and setup process
    Significant changes include:
    - Removed redundant file existence check in `copyFileFromTemplate` function
    - Swapped arguments order in `copyDataToFile` function for better readability
    - Added a new helper function `setup` to load environment variables and set up the application path
    - Improved error message when migration name is not specified in `doMake` function
    - Fixed template paths in `doMake` function

--- cmd/cli/copy-files.go
+++ cmd/cli/copy-files.go
@@ -9,18 +9,14 @@ import (
 var templateFS embed.FS
 
 func copyFileFromTemplate(templatePath, targetFile string) error {
-	// check if the file exists in the templateFS
-	_, err := templateFS.Open(templatePath)
-	if err != nil {
-		exitGracefully(err)
-	}
+	// TODO: check to ensure file does not already exist
 
 	data, err := templateFS.ReadFile(templatePath)
 	if err != nil {
 		exitGracefully(err)
 	}
 
-	err = copyDataToFile(targetFile, data)
+	err = copyDataToFile(data, targetFile)
 	if err != nil {
 		exitGracefully(err)
 	}
@@ -28,7 +24,7 @@ func copyFileFromTemplate(templatePath, targetFile string) error {
 	return nil
 }
 
-func copyDataToFile(targetFile string, data []byte) error {
+func copyDataToFile(data []byte, targetFile string) error {
 	err := os.WriteFile(targetFile, data, 0644)
 	if err != nil {
 		return err
--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -7,25 +7,29 @@ import (
 )
 
 func doMake(arg2, arg3 string) error {
+
 	switch arg2 {
 	case "migration":
 		dbType := cel.DB.DataType
 		if arg3 == "" {
-			exitGracefully(errors.New("you must specify a migration name"))
+			exitGracefully(errors.New("you must give the migration a name"))
 		}
+
 		fileName := fmt.Sprintf("%d_%s", time.Now().UnixMicro(), arg3)
+
 		upFile := cel.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
 		downFile := cel.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
 
-		err := copyFileFromTemplate("templates/migrations/migration."+dbType+".up.sql", upFile)
+		err := copyFileFromTemplate("templates/migrations/migrations."+dbType+".up.sql", upFile)
 		if err != nil {
 			exitGracefully(err)
 		}
 
-		err = copyFileFromTemplate("templates/migrations/migration."+dbType+".down.sql", downFile)
+		err = copyFileFromTemplate("templates/migrations/migrations."+dbType+".down.sql", downFile)
 		if err != nil {
 			exitGracefully(err)
 		}
 	}
+
 	return nil
 }
