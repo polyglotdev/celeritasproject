88ca59a271e55ecf1da363c4e28fed9a04d4339c
Dom Hallan <dom@domhallan.com>
2024-11-10 10:29:24 -0500

    :bug: Fixed path in template file copy function
    The path used in the `copyFileFromTemplate` function was corrected. Previously, it started with a `/`, which could lead to issues depending on the environment and directory structure. Now, it's relative to the current directory, improving compatibility across different setups.

--- cmd/cli/session.go
+++ cmd/cli/session.go
@@ -45,7 +45,7 @@ func doSessionTable() error {
 	upFile := cel.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
 	downFile := cel.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
 
-	err := copyFileFromTemplate("/templates/migrations/"+dbType+"_session.sql", upFile)
+	err := copyFileFromTemplate("templates/migrations/"+dbType+"_session.sql", upFile)
 	if err != nil {
 		exitGracefully(err)
 	}
